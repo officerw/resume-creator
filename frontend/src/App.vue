c2bc5472258b6410bc4f1998de05ca52a99cc00b
officerw <officerw@grinnell.edu>
2024-05-23 03:17:30 -0500

    Started creation of header and footer

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,85 +1,96 @@
+<!--
+  APP
+
+  This page consists of the visual information that will always remain on the app,
+  including a header, navbar, and footer.
+-->
+
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+import { ref } from 'vue'
 </script>
 
 <template>
+  <!--
+	  Header of the webpage is always present, regardless of the router view selected
+	  This way, the title and navbar are always present for the user to see and use
+  -->
   <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+    <div class="title">
+      <h1>Resume Creator</h1>
+    </div>
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
+    <div class="navbar">
       <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
+        <RouterLink to="/"><h3>Home</h3></RouterLink>
+        <RouterLink to="/editor"><h3>Resume Editor</h3></RouterLink>
       </nav>
     </div>
   </header>
 
   <RouterView />
+
+  <!--
+	  Footer is always present, containing information on the authorship of this webpage.
+  -->
+  <footer>
+    <p class="footer">
+      This website was created by William Officer, using Vue JS, Flask, and pdflatex. To reference the source code for this project, visit its <a href="https://github.com/officerw/resume-creator">GitHub repository</a>.
+    </p>
+  </footer>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
   header {
+    height: 6rem;
+    width: 100%;
+    padding: 0 10% 0 10%;
+    align-items: center;
     display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
+    background-color: #0461cf;
   }
 
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
+  header h1 {
+    color: white;
+    padding: 0 2rem 0 2rem;
   }
 
   nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
+    height: 100%;
+    width: 100%;
   }
-}
+
+  .navbar {
+    height: 100%;
+  }
+
+  nav a {
+    text-decoration: none;
+    color: #00bf7d;
+    padding: 0 2rem 0 2rem;
+    font-size: large;
+    height: 100%;
+  }
+
+  footer {
+    width: 100%;
+    bottom:0;
+    padding: 0 10% 0 10%;
+    height: 4rem;
+    background-color: #054fb9;
+    color: white;
+    text-align: center;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  footer a {
+    color: #00bf7d;
+    text-decoration: none;
+  }
+
+  footer a:hover {
+    background-color: rgba(0, 191, 125, 0.25);
+  }
 </style>
--- frontend/src/views/TemplateEditor.vue
+++ frontend/src/views/TemplateEditor.vue
@@ -1,7 +1,11 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+
 </script>
 
 <template>
-  
+
 </template>
+
+<style>
+
+</style>
