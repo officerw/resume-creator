935c09aabf0c6de832b983e7a7abf92a849f19ad
officerw <officerw@grinnell.edu>
2024-05-23 12:18:35 -0500

    Added left-right panels

--- frontend/src/views/TemplateEditor.vue
+++ frontend/src/views/TemplateEditor.vue
@@ -3,9 +3,37 @@
 </script>
 
 <template>
+    <div class="main">
+        <div id="left-panel">
 
+        </div>
+
+        <div id="right-panel">
+
+        </div>
+    </div>
 </template>
 
 <style>
+    .main {
+        width: 100%;
+        padding: 0 10% 0 10%;
+        height: 20px;
+        display: block;
+    }
+
+    #left-panel {
+        display: inline-block;
+        width: 50%;
+        height: 20px;
+        background-color: green;
+    }
+
+    #right-panel {
+        display: inline-block;
+        width: 50%;
+        height: 20px;
+        background-color: blue;
+    }
 
 </style>
