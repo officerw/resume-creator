e684921fffbe025a37d0a3023050b29581fda1dd
officerw <officerw@grinnell.edu>
2024-05-23 21:09:02 -0500

    Parsing resume json information in createpdf.py and compiling it into a string

--- backend/createpdf.py
+++ backend/createpdf.py
@@ -1,6 +1,134 @@
 from pdflatex import PDFLaTeX
+import json
 
+# We create a Resume object that consists of a name,
+# list of contact info, and a list of sections.
+#
+# In the json argument, we expect a json object with the
+# following structure:
+# {
+#     "name": "",
+#     "address": "",
+#     "contact-info": ["", "", ""],
+#     "sections": Section[]
+# }
+class Resume:
+    def __init__(self, json):
+        self.name = json["name"]
+        self.address = json["address"]
+        self.contact_info = json["contact-info"]
 
+        # Get a list of sections associated with this resume
+        sections = json["sections"]
+        self.sections = []
+        for section in sections:
+            self.sections.append(Section(section))
 
-pdfl = PDFLaTeX.from_texfile("my_file.tex")
-pdf, log, completed_process = pdfl.create_pdf(keep_pdf_file = True, keep_log_file = True)
+# We create a Section object that has a name
+# type (either lists or experiences), and content.
+# The content consists of either List's or Experience's
+#
+# In the json argument, we expect a json object with the
+# following structure:
+# {
+#     "section-name": "",
+#     "section-type": "lists",
+#     "content": [
+#         {
+#             "list-title": "",
+#             "list-content": ""
+#         }
+#     ]
+# }
+# or the following structure
+# {
+#     "section-name": "",
+#     "section-type": "experiences",
+#     "content": [
+#         {
+#             "experience-title": "",
+#             "location": "",
+#             "organization": "",
+#             "tenure": "",
+#             "content": ["", "", ""]
+#         }
+#     ]
+# }
+class Section:
+    def __init__(self, json):
+        self.section_name = json["section-name"]
+        self.is_experiences = json["section-type"] == "experiences"
+    
+        # Get the experience or list content associated with this section
+        self.content = []
+        if self.is_experiences:
+            for experience in json["content"]:
+                self.content.append(Experience(experience))
+        else:
+            for list in json["content"]:
+                self.content.append(List(list))
+
+# We create a List object that has a list
+# title and list of values associated with it
+#
+# In the json argument, we expect a json object with
+# the following structure
+# {
+#    "list-title": "",
+#    "list-content": ""
+# }
+class List:
+    def __init__(self, json):
+        self.list_title = json["list-title"]
+        self.list_content = json["list-content"]
+
+# We create an Experience object that
+# organizes experience title, location, tenure,
+# organization, and experience descriptions.
+#
+# In the json argument, we expect a json object with
+# the following structure
+# {
+#   "experience-title": "",
+#   "location": "",
+#   "organization": "",
+#   "tenure": "",
+#   "content": ["", "", ""]
+# }
+class Experience:
+    def __init__(self, json):
+        self.experience_title = json["experience-title"]
+        self.location = json["location"]
+        self.organization = json["organization"]
+        self.tenure = json["tenure"]
+        self.content = json["content"]
+
+def main():
+    file = open("resume-info.json")
+
+    data = Resume(json.load(file))
+
+    sections_info = ""
+    for section in data.sections:
+        sections_info += "\\sectionheader{" + section.section_name + "}\n\n"
+
+        if section.is_experiences:
+            for experience in section.content:
+                sections_info += "\\experienceheader{" + experience.experience_title + "}{" + experience.location + "}{" + experience.organization + "}{" + experience.tenure + "}\n\n"
+
+            sections_info += "\\begin{itemize}\n"
+            sections_info += "\\setlength\\itemsep{0pt}\n"
+
+            for item in section.content:
+
+        else:
+            sections_info += 
+
+        print(s.section_name)
+    #print((Section)(data.sections[0]).content)
+
+    #pdfl = PDFLaTeX.from_texfile("my_file.tex")
+    #pdf, log, completed_process = pdfl.create_pdf(keep_pdf_file = True, keep_log_file = True)
+
+if __name__ == "__main__":
+    main()
