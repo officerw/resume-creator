b6c8c55b80a3a21e9cf2a8b7761418b1a79484fb
officerw <officerw@grinnell.edu>
2024-05-27 01:27:21 -0500

    Adding draggable, deletable sections

--- frontend/src/components/ResumeSections.vue
+++ frontend/src/components/ResumeSections.vue
@@ -1,29 +1,48 @@
 
 <script setup lang="ts">
-    import { ref } from "vue"
+    import { ref, watch } from "vue"
+    import draggable from "vuedraggable"
+    import Section from "./Section.vue"
 
-    const sections = ref([])
-
-    // Add a section based on the type
-    function addExperienceSection(type: string) {
-        sections.value.push(type)
+    type SectionInfo = {
+        id: number
+        type: string
     }
 
+    // Define index and reactive list of section information
+    var index = 0
+    const sections = ref<SectionInfo[]>([])
+
+    // Add a section based on the type
+    function addSection(sectionType: string) {
+        sections.value.push({ id: ++index, type: sectionType})
+    }
+
+    // Remove a section when the user deletes it
+    function removeSection(idToRemove: Number) {
+        // Remove section to be removed by filtering out its id
+        sections.value = sections.value.filter((element) => element.id != idToRemove)
+    }
 
 </script>
 
 <template>
     <div class="resume-sections">
         <h4>Sections</h4>
-        <!-- The following section allows the user to add different kinds of sections to the resume builder -->
-        <div class="contact-list-buttons">
-            <button id="add-experience" @click="addSection("experience")">Add Experience Section</button>
-            <button id="add-list" @click="addSection("list")">Add List Section</button>
-        </div>
 
         <!-- List of sections -->
-        <div class="sections-list">
+        <div v-if="sections.length > 0" class="sections-list">
+            <draggable v-model="sections" item-key="id">
+                <template #item="{element}">
+                    <Section :id="element.id" :section-type="element.type" @delete-section="(idToRemove) => removeSection(idToRemove)"></Section>
+                </template>
+            </draggable>
+        </div>
 
+        <!-- The following section allows the user to add different kinds of sections to the resume builder -->
+        <div class="contact-list-buttons">
+            <button id="add-experience" @click="addSection('experience')">Add Experience Section</button>
+            <button id="add-list" @click="addSection('list')">Add List Section</button>
         </div>
     </div>
 </template>
---
+++ frontend/src/components/Section.vue
@@ -0,0 +1,47 @@
+
+<script setup lang="ts">
+    import { ref } from "vue"
+
+    const sectionTitle = ref("")
+
+    // Delete this section when requested
+    const emit = defineEmits(["deleteSection"])
+
+    // Accept whether this section is a list type or experience type
+    const props = defineProps({
+        sectionType: {
+            type: String,
+            required: true
+        },
+        id: {
+            type: Number,
+            required: true
+        }
+    })
+
+    // Delete this section
+    function deleteSection(idToRemove: number) {
+        emit("deleteSection", idToRemove)
+    }
+
+</script>
+
+<template>
+    <div class="resume-section-instance">
+        <h4>Section Title:</h4>
+        <button @click="deleteSection(id)">Delete Section</button>
+        <textarea id="section-title" v-model="sectionTitle" rows="1" name="sectionTitle" placeholder="Section Title Goes Here" maxlength="30"></textarea>
+
+        <!-- Here, we outline the layout of a list type section -->
+        <div v-if="sectionType == 'list'" class="list-section">
+            I am a list
+        </div>
+        <div v-else class="experience-section">
+            I am an experience
+        </div>
+    </div>
+</template>
+
+<style>
+
+</style>
