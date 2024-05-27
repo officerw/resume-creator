d91dfb9a89e9fe67e69afc2369c6b5fb9cf8b23c
officerw <officerw@grinnell.edu>
2024-05-26 23:33:23 -0500

    Working on adding sections

--- frontend/src/components/ResumeSections.vue
+++ frontend/src/components/ResumeSections.vue
@@ -1,21 +1,30 @@
-<!--
-  ResumeBuilder
-
-  This contains fields for the user to populate with information that would be
-  contained in their resume. This information is compiled into a Javascript
-  object and is sent to the TemplateEditor for processing.
--->
 
 <script setup lang="ts">
-    import NameContactInfo from "../components/NameContactInfo.vue"
-    import ResumeSections from "../components/ResumeSections.vue"
     import { ref } from "vue"
+
+    const sections = ref([])
+
+    // Add a section based on the type
+    function addExperienceSection(type: string) {
+        sections.value.push(type)
+    }
+
 
 </script>
 
 <template>
     <div class="resume-sections">
         <h4>Sections</h4>
+        <!-- The following section allows the user to add different kinds of sections to the resume builder -->
+        <div class="contact-list-buttons">
+            <button id="add-experience" @click="addSection("experience")">Add Experience Section</button>
+            <button id="add-list" @click="addSection("list")">Add List Section</button>
+        </div>
+
+        <!-- List of sections -->
+        <div class="sections-list">
+
+        </div>
     </div>
 </template>
 
